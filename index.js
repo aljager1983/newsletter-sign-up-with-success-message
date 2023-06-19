b2b1a1ffe61d4b938211176ea3f9b21fa97cdcf7
unknown <al_jager1983@yahoo.com>
2023-06-19 10:05:54 +0800

    updated js

--- index.js
+++ index.js
@@ -3,17 +3,20 @@ const successWindow = document.querySelector(".successWindow");
 const main = document.querySelector("main");
 const dismiss = document.querySelector(".dismissButton");
 const inputText = document.getElementById("email");
-
+const emailer = document.getElementById("emailer");
 const emailRegex = 
  new RegExp(/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/, "gm");
 
 
 subsButton.addEventListener("click", function(){
     
-    console.log("entered " + inputText.value );
+    console.log("entered " + inputText.value);
     const isValidEmail = emailRegex.test(inputText.value);
     if(isValidEmail == true) {
         console.log("valid email")
+        main.style.display = "none";
+        successWindow.style.display = "block";
+        emailer.innerHTML = inputText.value;
     } else {
         console.log("invalid email");
     }
