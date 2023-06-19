322b81d00df5e11cdd39da5893b74383e1f90ea1
promdiGamer <al_jager1983@yahoo.com>
2023-06-19 21:56:58 +0800

    for submission

--- index.js
+++ index.js
@@ -3,22 +3,25 @@ const successWindow = document.querySelector(".successWindow");
 const main = document.querySelector("main");
 const dismiss = document.querySelector(".dismissButton");
 const inputText = document.getElementById("email");
-const emailer = document.getElementById("emailer");
+
+const errorMsg = document.getElementById("errorMsg");
 const emailRegex = 
  new RegExp(/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/, "gm");
 
 
 subsButton.addEventListener("click", function(){
-    
+    const emailer = document.getElementById("emailer");
     console.log("entered " + inputText.value);
     const isValidEmail = emailRegex.test(inputText.value);
     if(isValidEmail == true) {
         console.log("valid email")
         main.style.display = "none";
-        successWindow.style.display = "block";
+        successWindow.style.display = "flex";
         emailer.innerHTML = inputText.value;
     } else {
         console.log("invalid email");
+        errorMsg.style.display = "block";
+        inputText.style.backgroundColor = "#FFE8E6";
     }
     
     
